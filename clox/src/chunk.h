9fe97112025bbe60f8d78380e70f6d8fb28cfeff
Jungy <jungorend@gmail.com>
2023-06-26 17:18:11 -0400

    Add constant operation

--- clox/src/chunk.h
+++ clox/src/chunk.h
@@ -2,19 +2,23 @@
 #define CHUNK_H_
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
   OP_RETURN,
+  OP_CONSTANT,
 } OpCode;
 
 typedef struct {
   int count;
   int capacity;
   uint8_t* code;
+  ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 
 #endif // CHUNK_H_
